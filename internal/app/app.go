b246b1b5d34cfbc4f79b9ef28d182e3a3ad77ad5
Artur Valiullin <ar.valiullin@yandex.ru>
2025-12-21 16:05:57 +0500

    implement AppConfig
    Реализовал обработку аргументов командной строки и переменных окружения.

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,25 +2,42 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"os"
 
+	"github.com/arvaliullin/gophermart/internal/config"
 	"github.com/rs/zerolog"
 )
 
 // App представляет основное приложение Gophermart.
 type App struct {
 	logger zerolog.Logger
+	config *config.AppConfig
 }
 
-func New(ctx context.Context) *App {
+func New(ctx context.Context) (*App, error) {
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		return nil, fmt.Errorf("ошибка загрузки конфигурации: %w", err)
+	}
+
 	logger := zerolog.New(os.Stdout).
 		With().
 		Timestamp().
 		Logger().
 		Level(zerolog.InfoLevel)
-	return &App{logger: logger}
+
+	return &App{
+		logger: logger,
+		config: cfg,
+	}, nil
 }
 
 func (a *App) Logger() *zerolog.Logger {
 	return &a.logger
+}
+
+func (a *App) Config() *config.AppConfig {
+	return a.config
 }
