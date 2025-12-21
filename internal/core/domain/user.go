c5fd38f9e3f70d24b92273e07c4152643707b278
Artur Valiullin <ar.valiullin@yandex.ru>
2025-12-21 18:43:36 +0500

    domain: добавлены доменные модели и ошибки
    - User: модель пользователя
    - Order: модель заказа со статусами (NEW, PROCESSING, INVALID, PROCESSED)
    - Balance: модель баланса пользователя
    - Withdrawal: модель списания баллов
    - errors: доменные ошибки (ErrUserNotFound, ErrOrderAlreadyExists и др.)

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,1 +1,11 @@
 package domain
+
+import "time"
+
+// User представляет пользователя системы лояльности.
+type User struct {
+	ID        int64
+	Login     string
+	Password  string
+	CreatedAt time.Time
+}
